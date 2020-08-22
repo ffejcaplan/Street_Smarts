b3338976fa0df42b1037d1e595be8b6c18bc27de
Ffej Caplan <ffej.caplan@gmail.com>
2020-08-22 11:58:00 -0400

    random comment

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,5 +4,5 @@ const isAuthenticated = require('../../config/middleware/isAuthenticated');
 router.get('/secrets', isAuthenticated, (req, res) => {
   res.json('Talk is cheap. Show me the code. -Linus Torvalds');
 });
-
+// random comment
 module.exports = router;
